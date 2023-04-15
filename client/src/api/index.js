3c03bad48a380890d462e927e1f0727ab8e63002
abhisheksharma <abhisheksharma131098@gmail.com>
2023-04-15 12:48:34 +0530

    build information added

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: process.env.REACT_APP_BEURL });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
